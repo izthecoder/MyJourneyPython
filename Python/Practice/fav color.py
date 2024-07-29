d0b2d38aaa439e8f1dbceb569e6545134d8579aa
Iz <176813758+izthecoder@users.noreply.github.com>
2024-07-30 07:44:55 +0800

    Update fav color.py

--- Python/Practice/fav color.py
+++ Python/Practice/fav color.py
@@ -3,7 +3,7 @@ name = input('Enter your name: ')
 print('what is your favourite color?')
 color = input('Enter your favourite color: ')
 
-print(f'Hi my nigga {name}, your fav color is {color}? awesome!')
+print(f'Hi {name}, your fav color is {color}? awesome!')
 
 
 
